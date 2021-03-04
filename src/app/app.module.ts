bc99c8c36098de09b538e5eb4a6cb9726f0948a7
scottkwon <scottkwon@gmail.com>
2021-03-03 19:07:26 -0800

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
 import { ProductDetailsComponent } from "./product-details/product-details.component";
 import { CartService } from "./cart.service";
-import { CartListComponent } from "./cart-list/cart-list.component";
 import { CartComponent } from "./cart/cart.component";
 
 @NgModule({
